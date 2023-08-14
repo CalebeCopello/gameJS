cd236fec5937d284be76af0394267f5340756790
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-08-14 16:09:46 -0300

    canvas set

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+const CONSTANTS={}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    CONSTANTS.CV = document.getElementById('display')
+    CONSTANTS.CTX = CONSTANTS.CV.getContext('2d')
+})
+
+
+export default CONSTANTS
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import CONSTANTS from './constants.js'
+
+document.addEventListener('DOMContentLoaded', () =>{
+    CONSTANTS.CV.width *= 1
+    CONSTANTS.CV.height *= 1
+})
