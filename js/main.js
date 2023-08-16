9463ad35718400b54cf614a018f3e4e23b102072
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-08-16 09:41:30 -0300

    movement ok

--- js/main.js
+++ js/main.js
@@ -12,13 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         displaySize(DISPLAYSIZE)
     }
-    // MEGAMAN.draw()
-
 })
 
 //classes
 class player {
-    constructor(posX,posY,width,height,velocityX,velocityY,aceleration) {
+    constructor(posX,posY,width,height,velocityX,velocityY,aceleration,jump=false) {
     this.posX = posX
     this.posY = posY
     this.width = width
@@ -26,12 +24,12 @@ class player {
     this.velocityX = velocityX
     this.velocityY = velocityY
     this.aceleration = aceleration
+    this.jump = jump
     }
     draw() {
-        CTX.scale(DISPLAYSIZE,DISPLAYSIZE)
         CTX.fillStyle = 'blue'
         CTX.fillRect(this.posX,this.posY,this.width,this.height)
-        // console.log(this.posX,this.posY,this.width,this.height)
+        console.log(this.velocityY)
     }
     update() {
         this.draw()
@@ -39,11 +37,30 @@ class player {
         this.posY += this.velocityY
         if (this.posY+this.height+this.velocityY <=CV.height) {
             this.velocityY += GRAVITY
-        } else { this.velocityY = 0}
+        } else { 
+            this.velocityY = 0
+            this.jump = false
+        }
+    }
+}
+
+class object {
+    constructor(posX,posY,width,height,damage=false) {
+        this.posX = posX
+        this.posY = posY
+        this.width = width
+        this.height = height
+        this.damage = damage
+    }
+    draw() {
+        CTX.fillStyle='green'
+        CTX.fillRect(this.posX,this.posY,this.width,this.height)
     }
 }
 
 const MEGAMAN = new player(50,50,24,24,0,0,1)
+const RECT = new object(160,180,48,16)
+
 //functions
 function displaySize(s=1) {
     CTX.scale(s,s)
@@ -55,57 +72,57 @@ function displaySize(s=1) {
     MEGAMAN.draw()
 }
 
-//controler
+//player movement
 document.addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'ArrowUp':
-            moveJump(5)
+            moveJump(-7)
             break
         case 'ArrowDown':
             console.log('down')
             break
         case 'ArrowLeft':
-            moveLeft(5)
+            moveLeft(-3)
             console.log('down')
             break
         case 'ArrowRight':
-            moveRight(5)
+            moveRight(+3)
             break
         default:
             console.log(`no command for ${key}`)
     }
 })
-// document.addEventListener('keyup', ({key}) => {
-//     switch (key) {
-//         case 'ArrowUp':
-//             moveJump(0)
-//             console.log('up')
-//             break
-//         case 'ArrowDown':
-
-//             break
-//         case 'ArrowLeft':
-//             moveLeft(0)
-//             console.log('up')
-//             break
-//         case 'ArrowRight':
-//             moveRight(0)
-//             break
-//         default:
-//             console.log(`no command for ${key}`)
-//     }
-// })
+document.addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case 'ArrowUp':
+            moveJump(0)
+            break
+        case 'ArrowDown':
+            break
+        case 'ArrowLeft':
+            moveLeft(0)
+            break
+        case 'ArrowRight':
+            moveRight(0)
+            break
+        default:
+            console.log(`no command for ${key}`)
+    }
+})
 
 function moveLeft (n) {
-    MEGAMAN.velocityX -= n
+    MEGAMAN.velocityX = n
 }
 
 function moveRight (n) {
-    MEGAMAN.velocityX += n
+    MEGAMAN.velocityX = n
 }
 
 function moveJump(n) {
-    MEGAMAN.velocityY -= n
+    if(!MEGAMAN.jump) {
+    MEGAMAN.velocityY = n
+    MEGAMAN.jump = true
+    }
 }
 
 //animation
@@ -113,5 +130,6 @@ function animation() {
     requestAnimationFrame(animation)
     CTX.clearRect(0,0,CV.width,CV.height)
     MEGAMAN.update()
+    RECT.draw()
 }
 animation()
