0d02f6417d6e788ad09e58136d81b9730691ca72
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-01-04 16:22:12 -0300

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,9 +2,48 @@
 const CV = document.getElementById('display')
 const CTX = CV.getContext('2d')
 const GRAVITY = 0.5
+const DISPLAYSIZE = 2
+
+//variables
+let PLAYERIMG = []
+let PLAYER
 
 
-//set display size
-const DISPLAYSIZE = 3
+//set display propierties
 CV.width = 256*DISPLAYSIZE
 CV.height = 224*DISPLAYSIZE
+CV.style.backgroundColor = 'rgb(255,255,255)'
+CTX.imageSmoothingEnabled = false;
+
+//function to set sprites
+function setPlayerSprites() {
+    PLAYERIMG = [new Image()]
+    PLAYERIMG[0].src = '/src/player/small/sm01.png'
+}
+
+function initGame() {
+    PLAYER = new player (1*DISPLAYSIZE,150*DISPLAYSIZE)
+}
+
+//player set
+class player {
+    constructor(posX,posY) {
+        this.posX = posX
+        this.posY = posY
+        this.image = PLAYERIMG[0]
+    }
+    draw() {
+        CTX.drawImage(this.image,this.posX,this.posY,16*2,16*2)
+    }
+    update() {
+    this.draw()
+    }
+}
+
+function animation() {
+    requestAnimationFrame(animation)
+    PLAYER.update()
+}
+setPlayerSprites()
+initGame()
+animation()
